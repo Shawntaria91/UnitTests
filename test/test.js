1a354b42124c002f680a70550d607a1ea6881c23
Shawntaria91 <shawntariaburden1991@gmail.com>
2020-10-26 20:47:31 -0400

    passing tests for mathenforcer

--- test/test.js
+++ test/test.js
@@ -1,8 +1,17 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+const chai = require('chai'); // boiler plate from chai
+var should = require('chai').should() // boiler plate from chai
+var assert = require('assert'); // boiler plate code from Mocha install
+
+let MathEnforcer = require("../mathEnforcer"); // imported our Class (object factory and will use it!)
+
+describe('AddFive Static Method', function() {      // Parameters are: (the text label, function to call test)
+    it('Total should equal 10 when the first parameter is the number 5', function() {
+      let total = MathEnforcer.addFive(5);         // total should be 10 (num + 5 = 10)
+      total.should.equal(10);
     });
-  });
+    it('total should equal 0 when the first parameter is the number -5', () => {
+      let total = MathEnforcer.addFive(-5);
+      total.should.equal(0);
+    });
+    
 });
